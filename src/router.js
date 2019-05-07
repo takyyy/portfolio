b498875b37cd29ea33cba473f86c21f13daa8cf0
takyyy <tkytkymtd@gmail.com>
2019-05-08 00:36:35 +0900

    bootstrap-vue 導入

--- src/router.js
+++ src/router.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default new Router({
   mode: 'history',
