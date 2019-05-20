54f793339456015b89c6aa0ca4b689edba5c5628
takyyy <tkytkymtd@gmail.com>
2019-05-21 00:30:43 +0900

    ABOUTページ雛形作成

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="aboout">
-        アバウト
+        <div class="card m-5 w-50 mx-auto shadow-sm">
+            <div class="card-body m-3">
+                <img class="rounded-circle float-right" src="/images/pathToYourImage.png" alt="Circle image">
+                <h4 class="card-title m-3">About Me</h4>
+                <p class="card-text m-5">テストテスト</p>
+            </div>
+        </div>
     </div>
 </template>
--- src/components/Navber.vue
+++ src/components/Navber.vue
@@ -1,22 +1,22 @@
-<<template>
-    <div>
+<template>
+    <div class="shadow">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mx-auto">
+            <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto m-2">
                 <li class="nav-item">
-                    <router-link class="text-white px-3" to="/">Home</router-link>
+                    <router-link class="text-white px-3" to="/">HOME</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="text-white px-3" to="/about">About</router-link>
+                    <router-link class="text-white px-3" to="/about">ABOUT</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="text-white px-3" to="/works">Works</router-link>
+                    <router-link class="text-white px-3" to="/works">WORKS</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="text-white px-3" to="/career">Career</router-link>
+                    <router-link class="text-white px-3" to="/career">CAREER</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="text-white px-3" to="/contact">Contact</router-link>
+                    <router-link class="text-white px-3" to="/contact">CONTACT</router-link>
                 </li>
                 </ul>
             </div>
